139bfd290411fca71f83738e04b31d365ad2cdf7
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-14 15:21:42 -0500

    Initial Commit

---
+++ Triangle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Triangle
+//
+//  Created by Logan Snopek on 2021-12-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Triangle/TriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleApp.swift
+//  Triangle
+//
+//  Created by Logan Snopek on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct TriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
