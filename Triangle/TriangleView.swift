53624c30ee2ba95900b17809f55f322c42beea96
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-14 19:34:47 -0500

    got the sliders and algorithm all done

--- Triangle/TriangleView.swift
+++ Triangle/TriangleView.swift
@@ -8,61 +8,77 @@
 import SwiftUI
 
 struct TriangleView: View {
+    
+    //MARK: Stored proprotys
+    
+    @State var side1: Double = 50
+    @State var side2: Double = 50
+    @State var side3: Double = 50
+    
+    //MARK: Computed proprotes
+    
+    var totalOfSides: Double {
+        return side1 + side2 + side3
+    }
+    
     var body: some View {
-        
-        VStack {
-        
-            Text("Side 1")
-                .font(.title2)
-                .bold()
-            
-        Slider(value: .constant(50.0),
-               in: 0.0...100.0,
-               label: {
+        ScrollView {
+            VStack {
+                
+                Text("Side 1")
+                    .font(.title2)
+                    .bold()
+                
+                Slider(value: $side1,
+                       in: 0.0...179.0,
+                       label: {
                     Text("Opacity")
-        },
-               minimumValueLabel: {
+                },
+                       minimumValueLabel: {
                     Text("0")
-        },
-               maximumValueLabel: {
-                    Text("100")
-        })
-                .padding()
-            
-            Text("Side 2")
-                .font(.title2)
-                .bold()
-        
-        Slider(value: .constant(50.0),
-               in: 0.0...100.0,
-               label: {
+                },
+                       maximumValueLabel: {
+                    Text("179")
+                })
+                    .padding()
+                
+                Text("Side 2")
+                    .font(.title2)
+                    .bold()
+                
+                Slider(value: $side2,
+                       in: 0.0...179.0,
+                       label: {
                     Text("Opacity")
-        },
-               minimumValueLabel: {
+                },
+                       minimumValueLabel: {
                     Text("0")
-        },
-               maximumValueLabel: {
-                    Text("100")
-        })
-                .padding()
-            
-            Text("Side 3")
-                .font(.title2)
-                .bold()
-        
-        Slider(value: .constant(50.0),
-               in: 0.0...100.0,
-               label: {
+                },
+                       maximumValueLabel: {
+                    Text("179")
+                })
+                    .padding()
+                
+                Text("Side 3")
+                    .font(.title2)
+                    .bold()
+                
+                Slider(value: $side3,
+                       in: 0.0...179.0,
+                       label: {
                     Text("Opacity")
-        },
-               minimumValueLabel: {
+                },
+                       minimumValueLabel: {
                     Text("0")
-        },
-               maximumValueLabel: {
-                    Text("100")
-        })
-                .padding()
-            
+                },
+                       maximumValueLabel: {
+                    Text("179")
+                })
+                    .padding()
+                
+                Text("\(totalOfSides)")
+                
+            }
         }
     }
 }
