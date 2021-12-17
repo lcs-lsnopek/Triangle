8b5efb2bc24b5f0939cac12428a573cd19c91423
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-16 23:09:40 -0500

    got all the logic done added a group because I ran out of figures just a tuneup now

--- Triangle/TriangleView.swift
+++ Triangle/TriangleView.swift
@@ -11,9 +11,9 @@ struct TriangleView: View {
     
     //MARK: Stored proprotys
     
-    @State var side1: Double = 50
-    @State var side2: Double = 50
-    @State var side3: Double = 50
+    @State var side1: Double = 60
+    @State var side2: Double = 60
+    @State var side3: Double = 60
     
     //MARK: Computed proprotes
     
@@ -24,62 +24,91 @@ struct TriangleView: View {
     var body: some View {
         ScrollView {
             VStack {
-                
-                Text("Side 1")
-                    .font(.title2)
-                    .bold()
-                
-                Slider(value: $side1,
-                       in: 0.0...179.0,
-                       label: {
-                    Text("Opacity")
-                },
-                       minimumValueLabel: {
-                    Text("0")
-                },
-                       maximumValueLabel: {
-                    Text("179")
-                })
-                    .padding()
-                
-                Text("Side 2")
-                    .font(.title2)
-                    .bold()
-                
-                Slider(value: $side2,
-                       in: 0.0...179.0,
-                       label: {
-                    Text("Opacity")
-                },
-                       minimumValueLabel: {
-                    Text("0")
-                },
-                       maximumValueLabel: {
-                    Text("179")
-                })
-                    .padding()
-                
-                Text("Side 3")
-                    .font(.title2)
-                    .bold()
-                
-                Slider(value: $side3,
-                       in: 0.0...179.0,
-                       label: {
-                    Text("Opacity")
-                },
-                       minimumValueLabel: {
-                    Text("0")
-                },
-                       maximumValueLabel: {
-                    Text("179")
-                })
-                    .padding()
+                Group {
+                    Text("Angle 1")
+                        .font(.title2)
+                        .bold()
+                    
+                    Text("\(side1)")
+                        .bold()
+                    
+                    Slider(value: $side1,
+                           in: 0.0...179.0,
+                           label: {
+                        Text("Opacity")
+                    },
+                           minimumValueLabel: {
+                        Text("0")
+                    },
+                           maximumValueLabel: {
+                        Text("179")
+                    })
+                        .padding()
+                    
+                    Text("Angle 2")
+                        .font(.title2)
+                        .bold()
+                    Text("\(side2)")
+                    
+                    Slider(value: $side2,
+                           in: 0.0...179.0,
+                           label: {
+                        Text("Opacity")
+                    },
+                           minimumValueLabel: {
+                        Text("0")
+                    },
+                           maximumValueLabel: {
+                        Text("179")
+                    })
+                        .padding()
+                    
+                    Text("\(side3)")
+                        .bold()
+                    
+                    Text("Angle 3")
+                        .font(.title2)
+                        .bold()
+                    
+                    Slider(value: $side3,
+                           in: 0.0...179.0,
+                           label: {
+                        Text("Opacity")
+                    },
+                           minimumValueLabel: {
+                        Text("0")
+                    },
+                           maximumValueLabel: {
+                        Text("179")
+                    })
+                        .padding()
+                    
+                }
                 
                 Text("\(totalOfSides)")
                 
                 if totalOfSides > 180 {
                     Text("Error")
+                        .bold()
+                }
+                if totalOfSides < 180 {
+                    Text("Error")
+                        .bold()
+                }
+                if totalOfSides / 3 == side1 {
+                    Text("Equlaterl")
+                        .bold()
+                }
+                if totalOfSides < side1 {
+                    Text("Isosales")
+                        .bold()
+                }
+                if totalOfSides < side2 {
+                    Text("Isosales")
+                        .bold()
+                }
+                if totalOfSides < side3 {
+                    Text("Isosales")
                         .bold()
                 }
                 
